d4361c46e1e4d3b63f30aac819f6aa4a4716370e
brandbern <berningbran9@gmail.com>
2023-06-02 16:24:13 -0400

    more content changes

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,4 +1,9 @@
-export default function PageBlog () {
+import Content from '@slimplate/content'
+
+export default async function PageBlog () {
+  const content = new Content('blog')
+  const blogs = await Promise.all((await content.list()).map(f => content.get(f)))
+  console.log('blogs', blogs)
   return (
     <main>
       <div>
--- src/content.js
+++ src/content.js
@@ -1,28 +1,25 @@
-import frontmatter from 'frontmatter'
+import { glob } from 'glob'
 import { readFileSync } from 'fs'
+import frontmatter from 'frontmatter'
+import { readFile } from 'fs/promises'
 
 export default class Content {
   constructor (collectionName, basePath = '.') {
     const { collections } = JSON.parse(readFileSync(basePath + '/.slimplate.json', 'utf8'))
-    this.files = collections[collectionName].files
+    this.collection = collections[collectionName]
     this.basePath = basePath
+    this.collectionName = collectionName
   }
 
   // get all filenames that matches files pattern in def
   async list () {
-    const { glob } = await import('glob')
-    return (await glob(this.basePath + this.files)).map(f => '/' + f)
+    return (await glob(this.basePath + this.collection.files)).map(f => '/' + f)
   }
 
   // get a single article, keyed by filename
   async get (filename) {
-    const { readFile } = await import('fs/promises')
     const { data, content } = frontmatter(await readFile(this.basePath + filename, 'utf8'))
+    data.url = this.collection.url
     return { ...data, children: content }
   }
 }
-
-const content = new Content('blog')
-
-console.log(await content.list())
-console.log(await content.get('/content/blog/2023-03-08-first.mdx'))
