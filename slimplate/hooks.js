dd6412ab6b4d9d69385f4092cd7f7d90c1298286
David Konsumer <konsumer@jetboystudio.com>
2023-06-06 12:36:44 -0700

    more stuff around front/backend

--- slimplate/hooks.js
+++ slimplate/hooks.js
@@ -1,11 +1,25 @@
+async function setupGit (collection) {
+  // TODO: if no user in localstorage, just quit
+  // TODO: create shared git if user is logged in, use collection to set things up
+}
+
 export function useSlimplate (collection) {
   return {
     async getClientsideList () {
+      const git = await setupGit(collection)
+      if (!git) {
+        return []
+      }
+
       // TODO: get list of all content from git
-      return []
     },
 
     async getClientsideItem (filename) {
+      const git = await setupGit(collection)
+      if (!git) {
+        return null
+      }
+
       // TODO: get single item from git
     }
   }
