66da64954b24b88e8392f8e41a8b289f731ff3b1
David Konsumer <konsumer@jetboystudio.com>
2023-06-06 15:23:31 -0700

    better navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import { ButtonLogin } from '@slimplate/daisyui'
 export default function App ({ Component, pageProps }) {
   return (
     <>
-      <nav className='navbar bg-base-100'>
+      <nav className='navbar bg-base-200 mb-4'>
         <a className='btn btn-ghost normal-case text-xl'>Slimplate Demo Site</a>
         <div className='ml-auto'>
           <ButtonLogin />
