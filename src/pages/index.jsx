4e97fe61a0c7422ba1d36d8a015d6c98f10f96ce
David Konsumer <konsumer@jetboystudio.com>
2023-06-06 15:55:35 -0700

    use Link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
+import Link from 'next/link'
+
 export default function Home () {
   return (
     <main className='prose m-auto'>
       <h2>Welcome</h2>
-      <p>Check out out <a href='/blog'>blog</a>.</p>
+      <p>Check out out <Link href='/blog'>blog</Link>.</p>
     </main>
   )
 }
