f10bf5ff8bfd0e8d0e1abe0e5e0d35b3e74b7cbc
David Konsumer <konsumer@jetboystudio.com>
2023-06-06 17:10:08 -0700

    better sidebar

--- slimplate/daisyui/EditorPage.jsx
+++ slimplate/daisyui/EditorPage.jsx
@@ -11,15 +11,15 @@ export default function EditorPage ({ item, children }) {
   }
 
   return (
-    <div className='drawer drawer-end'>
+    <div className='drawer'>
       <input id='slimplate-drawer' type='checkbox' className='drawer-toggle' />
       <div className='drawer-content'>
-        <label htmlFor='slimplate-drawer' className='fixed top-2 right-2 btn drawer-button'><IconPencil>Edit</IconPencil></label>
+        <label htmlFor='slimplate-drawer' className='sticky top-2 btn drawer-button'><IconPencil>Edit</IconPencil></label>
         {children}
       </div>
       <div className='drawer-side'>
         <label htmlFor='slimplate-drawer' className='drawer-overlay' />
-        <div className='menu p-4 w-80 h-full bg-base-200 text-base-content overflow-hidden'>
+        <div className='menu p-4 w-80 h-full bg-base-200 text-base-content'>
           <label htmlFor='slimplate-drawer' className='btn btn-primary mt-4'>COOL!</label>
           <h2>EDITOR HERE</h2>
         </div>
