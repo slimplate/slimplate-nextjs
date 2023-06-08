2de9e56eb6af006d38316f854ed980d72b81a890
David Konsumer <konsumer@jetboystudio.com>
2023-06-08 10:37:10 -0700

    merge

--- slimplate/daisyui/EditorPage.jsx
+++ slimplate/daisyui/EditorPage.jsx
@@ -1,27 +1,23 @@
 // Client-side editor drawer
+import { useRef } from 'react'
 import { IconPencil, IconPlus } from '@tabler/icons-react'
-import { useLocalStorage, tt } from '@slimplate/utils'
+import { useLocalStorage } from '@slimplate/utils'
 import Form from './Form'
 import Git from '@slimplate/github'
-import s from '@/../.slimplate.json'
-
-const { repo, branch } = s
 
 export default function EditorPage ({ item, collection, children }) {
   const [user] = useLocalStorage('user', false)
+  const r = useRef()
 
   if (!user) {
     return children
   }
 
-  // const remote = (await git.fetch()).fetchHead
-  //      const log = await git.log()
-  //      const local = log[0].oid
-  // const diff = await git.diff(remote, local)
-  const dirname = (f) => f.split('/').slice(0, -1).join('/')
+  const handleSubmit = e => {
+    e.preventDefault()
+    const values = Object.fromEntries(new FormData(e.currentTarget).entries())
+    console.log(values)
 
-  const handleSubmit = () => {
-    console.log(collection.filename)
     const git = new Git({ collection, repo, proxy: process.env.NEXT_PUBLIC_CORS_PROXY, branch: branch || 'main' })
     git.init().then(async () => {
       if (git.updated) {
@@ -35,11 +31,14 @@ export default function EditorPage ({ item, collection, children }) {
         await git.push()
       }
     })
+
+    // close window
+    r.current.checked = false
   }
 
   return (
     <div className='drawer'>
-      <input id='slimplate-drawer' type='checkbox' className='drawer-toggle' />
+      <input ref={r} id='slimplate-drawer' type='checkbox' className='drawer-toggle' />
       <div className='drawer-content'>
         <label htmlFor='slimplate-drawer' className='sticky top-2 left-2 btn drawer-button'>
           {item
@@ -50,10 +49,11 @@ export default function EditorPage ({ item, collection, children }) {
       </div>
       <div className='drawer-side'>
         <label htmlFor='slimplate-drawer' className='drawer-overlay' />
-        <div className='menu h-full bg-base-200 text-base-content flex flex-col gap-4'>
+        <form className='menu h-full bg-base-200 text-base-content flex flex-col gap-4' onSubmit={handleSubmit}>
           <Form collection={collection} item={item} />
-          <label onClick={handleSubmit} htmlFor='slimplate-drawer' className='btn btn-primary mt-4'>{item ? 'Update' : 'Create'}</label>
-        </div>
+          <label htmlFor='slimplate-drawer' className='btn mt-4'>Cancel</label>
+          <button type='submit' className='btn btn-primary mt-4'>{item ? 'Update' : 'Create'}</button>
+        </form>
       </div>
     </div>
   )
