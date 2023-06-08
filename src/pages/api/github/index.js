d447d4a8aa0c1991bcfe0b72070698a89641199b
David Konsumer <konsumer@jetboystudio.com>
2023-06-07 19:59:21 -0700

    use Response for errors

--- src/pages/api/github/index.js
+++ src/pages/api/github/index.js
@@ -9,13 +9,13 @@ const { REDIRECT_URL, GITHUB_CLIENT, GITHUB_SECRET } = process.env
 
 export default function handler (req, res) {
   if (!REDIRECT_URL) {
-    return res.status(500).json({ error: true, message: 'set REDIRECT_URL' })
+    return new Response('set REDIRECT_URL', { status: 500 })
   }
   if (!GITHUB_CLIENT) {
-    return res.status(500).json({ error: true, message: 'set GITHUB_CLIENT' })
+    return new Response('set GITHUB_CLIENT', { status: 500 })
   }
   if (!GITHUB_SECRET) {
-    return res.status(500).json({ error: true, message: 'set GITHUB_SECRET' })
+    return new Response('set GITHUB_SECRET', { status: 500 })
   }
 
   return ghlogin(req, res, REDIRECT_URL, GITHUB_CLIENT)
