623258e0be5020810c16082c1d7766f30afa213d
brandbern <berningbran9@gmail.com>
2023-06-08 12:43:56 -0400

    working on pushing changes

--- slimplate/daisyui/EditorPage.jsx
+++ slimplate/daisyui/EditorPage.jsx
@@ -1,13 +1,32 @@
 // Client-side editor drawer
 import { IconPencil, IconPlus } from '@tabler/icons-react'
-import { useLocalStorage } from '@slimplate/utils'
+import { useLocalStorage, tt } from '@slimplate/utils'
 import Form from './Form'
+import Git from '@slimplate/github'
+import s from '@/../.slimplate.json'
+
+const { repo, branch } = s
 
 export default function EditorPage ({ item, collection, children }) {
   const [user] = useLocalStorage('user', false)
 
   if (!user) {
     return children
+  }
+
+  const handleSubmit = () => {
+    console.log(collection.filename)
+    const git = new Git({ collection, repo, proxy: process.env.NEXT_PUBLIC_CORS_PROXY, branch: branch || 'main' })
+    git.init().then(async () => {
+      if (git.updated) {
+        console.log(collection)
+        await git.pull()
+        const filename = tt(collection.filename, { ...item, date: item.date.substring(0, 10) })
+        await git.add(filename)
+        await git.commit('making test')
+        await git.push()
+      }
+    })
   }
 
   return (
@@ -25,7 +44,7 @@ export default function EditorPage ({ item, collection, children }) {
         <label htmlFor='slimplate-drawer' className='drawer-overlay' />
         <div className='menu h-full bg-base-200 text-base-content flex flex-col gap-4'>
           <Form collection={collection} item={item} />
-          <label htmlFor='slimplate-drawer' className='btn btn-primary mt-4'>{item ? 'Update' : 'Create'}</label>
+          <label onClick={handleSubmit} htmlFor='slimplate-drawer' className='btn btn-primary mt-4'>{item ? 'Update' : 'Create'}</label>
         </div>
       </div>
     </div>
--- slimplate/daisyui/Form.jsx
+++ slimplate/daisyui/Form.jsx
@@ -3,10 +3,10 @@ import dateFormat from 'dateformat'
 
 const inputClass = 'p-2 bg-base-100 border border-neutral-500/20 rounded-md w-full'
 export const widgetMap = {
-  richtext: ({ value, name }) => <textarea id={name} name={name} className={cx(inputClass)} rows='10' cols='40' defaultValue={value} />,
-  date: ({ value, name }) => <input id={name} name={name} className={cx(inputClass)} type='date' defaultValue={dateFormat(value, 'yyyy-mm-dd')} />,
-  color: ({ value, name }) => <input id={name} name={name} className={cx(inputClass)} type='color' defaultValue={value || '#FFFFFF'} />,
-  default: ({ value, name }) => <input id={name} name={name} className={cx(inputClass)} type='text' defaultValue={value} />
+  richtext: ({ value, onChange, name }) => <textarea id={name} name={name} className={cx(inputClass)} rows='10' cols='40' defaultValue={value} onChange={e => onChange(e.target.value)} />,
+  date: ({ value, onChange, name }) => <input id={name} name={name} className={cx(inputClass)} type='date' defaultValue={dateFormat(value, 'yyyy-mm-dd')} onChange={e => onChange(e.target.value)} />,
+  color: ({ value, onChange, name }) => <input id={name} name={name} className={cx(inputClass)} type='color' defaultValue={value || '#FFFFFF'} onChange={e => onChange(e.target.value)} />,
+  default: ({ value, onChange, name }) => <input id={name} name={name} className={cx(inputClass)} type='text' defaultValue={value} onChange={e => onChange(e.target.value)} />
 }
 
 export default function Form ({ collection, item }) {
@@ -14,10 +14,15 @@ export default function Form ({ collection, item }) {
     const field = collection.fields[name]
     const value = item ? item[name] : ''
     const InputElement = widgetMap[field.type] || widgetMap.default
+
+    const handleChange = (newVal) => {
+      item[name] = newVal
+    }
+
     return (
       <div key={name}>
         <label htmlFor={name}>{field.label}:</label>
-        <InputElement value={value} name={name} />
+        <InputElement onChange={handleChange} value={value} name={name} />
       </div>
     )
   })
